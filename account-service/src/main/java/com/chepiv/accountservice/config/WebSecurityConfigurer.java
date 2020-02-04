b1200dad99ddd6aac0b55db5c6de1fc56fc4d72b
Ivan Chepurin <ivan.chepurin@satlan.pl>
2020-02-04 12:44:50 +0100

    cors not working

--- account-service/src/main/java/com/chepiv/accountservice/config/WebSecurityConfigurer.java
+++ account-service/src/main/java/com/chepiv/accountservice/config/WebSecurityConfigurer.java
@@ -6,11 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
@@ -39,6 +43,22 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/accounts/register","accounts/login");
+    }
+
+    @Bean
+    public CorsFilter corsFilter() { //doesnt work
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
 
--- ui-service/src/app/login/login.component.ts
+++ ui-service/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -11,6 +11,7 @@ import { Observable } from 'rxjs';
 export class LoginComponent implements OnInit {
 
   model: any = {};
+  token: any = {};
 
   constructor(
     private route: ActivatedRoute,
@@ -23,18 +24,35 @@ export class LoginComponent implements OnInit {
   }
 
   sendLoginRequest() {
-    const url = 'http://localhost:8762/accounts/login';
-    this.http.post<Observable<boolean>>(url, {
-      login: this.model.login,
-      password: this.model.password
-    }).subscribe(isValid => {
-      if (isValid) {
-        sessionStorage.setItem('token', btoa(this.model.login + ':' + this.model.password));
-        sessionStorage.setItem('currentLogin', this.model.login);
-        this.router.navigate(['/user-details']);
-      } else {
-        alert('Authentication failed.');
-      }
-    });
+    const url = 'http://localhost:8762/oauth/token';
+
+    const headers = {
+      Authorization: 'Basic ' + btoa('eagleeye' + ':' + 'thisissecret'),
+      'Content-type': 'application/x-www-form-urlencoded'
+    };
+
+    const body = new HttpParams() // fix cors policy
+      .set('username', this.model.login)
+      .set('password', this.model.password)
+      .set('grant_type', 'webclient')
+      .set('grant_type', 'password');
+
+    this.http.post('http://localhost:8080/' + 'oauth/token', body, {headers})
+      .subscribe(data => this.token = data.token,
+        err => alert('invalid Creadtilas'));
+
+
+    // this.http.post<Observable<boolean>>(url, {
+    //   login: this.model.login,
+    //   password: this.model.password
+    // }).subscribe(isValid => {
+    //   if (isValid) {
+    //     sessionStorage.setItem('token', btoa(this.model.login + ':' + this.model.password));
+    //     sessionStorage.setItem('currentLogin', this.model.login);
+    //     this.router.navigate(['/user-details']);
+    //   } else {
+    //     alert('Authentication failed.');
+    //   }
+    // });
   }
 }
