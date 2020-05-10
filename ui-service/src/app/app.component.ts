ad6941815c495643f4a35df1de504b1f15016d80
Ivan Chepurin <ivan.chepurin@satlan.pl>
2020-05-10 23:49:05 +0200

    small fix rediresct

--- ui-service/src/app/app.component.ts
+++ ui-service/src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'games-xchanger';
+  private readonly token: string;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private http: HttpClient,
+              private toastr: ToastrService) {
+    this.token = sessionStorage.getItem('token');
+  }
+
+  ngOnInit() {
+    if (this.token == null) {
+      this.router.navigate(['/login']);
+    }
+  }
 
   logout() {
     sessionStorage.clear();
   }
+
 }
