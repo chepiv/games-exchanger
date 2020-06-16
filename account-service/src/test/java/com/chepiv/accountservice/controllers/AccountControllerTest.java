ef0d147cbef576d87e7ce0c831b790b46583e2f5
Ivan Chepurin <ivan.chepurin@satlan.pl>
2020-06-16 19:17:37 +0200

    remove docs

---
+++ account-service/src/test/java/com/chepiv/accountservice/controllers/AccountControllerTest.java
@@ -0,0 +1,41 @@
+package com.chepiv.accountservice.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllAccounts() {
+    }
+
+    @Test
+    void createAccount() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void user() {
+    }
+
+    @Test
+    void userDetails() {
+    }
+
+    @Test
+    void getUserByLogin() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+}
